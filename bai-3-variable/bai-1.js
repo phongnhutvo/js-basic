b7c9f69e85ed049f0373476ef072376f35d77f63
phongnhutvo <phongnhutvo@gmail.com>
2020-05-31 02:21:57 +0700

    variable

---
+++ bai-3-variable/bai-1.js
@@ -0,0 +1,10 @@
+var x = 1, y = 2;
+var sum = x + y;
+var sub = x - y;
+var mul = x * y;
+var div = x / y;
+
+console.log("sum: ", sum);
+console.log("sub: ", sub);
+console.log("mul: ", mul);
+console.log("div: ", div);
